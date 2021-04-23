c675f1f2b0f83d50681121d8563ad1d894476116
Akira <wonderful_wave_0608@yahoo.co.jp>
2021-04-24 00:41:42 +0900

    ニューヨーク・タイムス

---
+++ times/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+p {
+  code {
+    color: #0f0;
+  }
+}
+</style>
